f620f70af661a1d4f9f359b639c545d0af07ee86
Thomas Binetruy <thomas.binetruy@telecom-paristech.fr>
2018-10-09 14:49:51 +0200

    updated slave.jar with Thread.wait(10000)

--- TP/TP1/src/Slave.java
+++ TP/TP1/src/Slave.java
@@ -1,5 +1,15 @@
+// jar cmf Slave.mf Slave.jar Slave.class Slave.java
+
+import java.lang.Thread;
+import java.lang.InterruptedException;
+
 public class Slave {
     public Slave() {
+        try {
+            Thread.sleep(10000);
+        } catch(InterruptedException e) {
+            System.err.println("Thread error.");
+        }
         System.out.println(1 + 2);
     }
     public static void main(String[] args) {
