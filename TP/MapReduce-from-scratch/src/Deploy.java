5a5b1dfeda23453605e1d8e5623abb042293fcc1
Thomas Binetruy <thomas.binetruy@telecom-paristech.fr>
2018-10-09 21:01:36 +0200

    added deploy and slave codes

---
+++ TP/MapReduce-from-scratch/src/Deploy.java
@@ -0,0 +1,71 @@
+import java.io.InputStream;
+import java.io.BufferedInputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.lang.InterruptedException;
+
+public class Deploy {
+    public Deploy() {
+        ArrayList<String> list_m = new ArrayList<>();
+        ArrayList<Process> list_p = new ArrayList<>();
+        list_m.add("c133-07");
+        list_m.add("c133-08");
+        list_m.add("c133-09");
+
+        for(int i = 0; i < list_m.size(); i++) {
+            ProcessBuilder pb = new ProcessBuilder("ssh",
+                                                   "binetruy@" + list_m.get(i),
+                                                   "hostname");
+
+            try {
+                Process p = pb.start();
+                list_p.add(p);
+            } catch (IOException e) {
+                System.err.println("An error has occurred while starting the process " + Integer.toString(i) + ".");
+            }
+        }
+
+        for(Process p : list_p) {
+            try {
+                p.waitFor();
+            } catch(InterruptedException e) {
+                System.err.println("An error has occurred while waiting for a process.");
+            }
+        }
+
+        for(Process p : list_p) {
+            this.readOutput(p);
+        }
+    }
+    public void inputString2String(InputStream is, boolean isError) {
+        BufferedInputStream bis = new BufferedInputStream(is);
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+
+        try {
+            String line = br.readLine();
+            while(line != null) {
+                if(!isError)
+                    System.out.println(line);
+                else
+                    System.err.println("e: " + line);
+
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.err.println("Error while reading process output.");
+        }
+    }
+    public void readOutput(Process p) {
+        InputStream is = p.getInputStream();
+        InputStream is2 = p.getErrorStream();
+
+        this.inputString2String(is, false);
+        this.inputString2String(is2, true);
+    }
+    public static void main(String[] args) {
+        Deploy m = new Deploy();
+    }
+}
---
+++ TP/MapReduce-from-scratch/src/Slave.java
@@ -0,0 +1,16 @@
+import java.lang.Thread;
+import java.lang.InterruptedException;
+
+public class Slave {
+    public Slave() {
+        try {
+            Thread.sleep(10000);
+        } catch(InterruptedException e) {
+            System.err.println("Thread error.");
+        }
+        System.out.println(1 + 2);
+    }
+    public static void main(String[] args) {
+        Slave m = new Slave();
+    }
+}
