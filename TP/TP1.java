06534bb9928f3e23d91fc5141b353aa0792ea8b8
Thomas Binetruy <thomas.binetruy@telecom-paristech.fr>
2018-10-15 11:46:38 +0200

    deleted obs files

--- TP/TP1.java
+++
@@ -1,9 +0,0 @@
-// TP1
-public class TP1 {
-
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
-        String content = new String(Files.readAllBytes(Paths.get("words.dat")));
-    }
-}
